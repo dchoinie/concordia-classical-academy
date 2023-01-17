246242fc8d723cdb8faf1ad5ac5eaedbbab6a600
Dan Choiniere <dchoiniere@exclamationlabs.com>
2023-01-17 15:55:11 -0600

    Fix staff query

--- site/src/pages/about/staff.tsx
+++ site/src/pages/about/staff.tsx
@@ -1,21 +1,54 @@
+import { graphql } from "gatsby";
 import React from "react";
 import Layout from "../../components/layout";
 import PageTitle from "../../components/pageTitle";
 import { SEO } from "../../components/seo";
-import StaffComponent from "../../components/staff";
+import StaffCard from "../../components/staffCard";
 
-const Staff = () => {
+const Staff = ({ data }: any) => {
   return (
     <>
       <SEO title="Staff" />
       <Layout>
         <div className="max-w-screen-xl mx-auto">
           <PageTitle title="Staff" />
-          <StaffComponent />
+          <div className="my-24 grid grid-cols-4 gap-12">
+            {data.staff.edges.map((staff: any) => (
+              <StaffCard
+                key={staff.node.name}
+                email={staff.node.email}
+                name={staff.node.name}
+                position={staff.node.position}
+                title={staff.node.title}
+                imageUrl={staff.node.headshot.asset.url}
+              />
+            ))}
+          </div>
         </div>
       </Layout>
     </>
   );
 };
+
+export const query = graphql`
+  query MyQuery {
+    staff: allSanityStaffMember(sort: { fields: name, order: ASC }) {
+      edges {
+        node {
+          id
+          email
+          name
+          position
+          title
+          headshot {
+            asset {
+              url
+            }
+          }
+        }
+      }
+    }
+  }
+`;
 
 export default Staff;

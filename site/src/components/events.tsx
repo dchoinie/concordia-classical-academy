be5f8052154e23ba068766fed8b29f9df952bd95
Dan Choiniere <dchoinie@gmail.com>
2023-12-08 08:19:52 -0600

    Fix home calendar link

--- site/src/components/events.tsx
+++ site/src/components/events.tsx
@@ -1,9 +1,9 @@
-import React from 'react';
-import { graphql, Link, useStaticQuery } from 'gatsby';
-import Button from './button/button';
-import { faCalendarDays } from '@fortawesome/free-solid-svg-icons';
-import { EventType } from '../types/common';
-import EventsCard from './eventsCard';
+import React from "react";
+import { graphql, Link, useStaticQuery } from "gatsby";
+import Button from "./button/button";
+import { faCalendarDays } from "@fortawesome/free-solid-svg-icons";
+import { EventType } from "../types/common";
+import EventsCard from "./eventsCard";
 
 const Events = (): JSX.Element => {
   const data = useStaticQuery(graphql`
@@ -28,25 +28,25 @@ const Events = (): JSX.Element => {
   `);
 
   return (
-    <section className='py-12 bg-white sm:py-16 lg:py-20'>
-      <div className='px-6 mx-auto max-w-screen-xl sm:px-6 lg:px-8'>
-        <div className='max-w-md mx-auto text-center'>
-          <h2 className='text-3xl font-bold text-primary sm:text-4xl font-pj'>
+    <section className="py-12 bg-white sm:py-16 lg:py-20">
+      <div className="px-6 mx-auto max-w-screen-xl sm:px-6 lg:px-8">
+        <div className="max-w-md mx-auto text-center">
+          <h2 className="text-3xl font-bold text-primary sm:text-4xl font-pj">
             Upcoming Events
           </h2>
-          <p className='mt-5 font-normal text-gray-600 font-pj mb-4'>
+          <p className="mt-5 font-normal text-gray-600 font-pj mb-4">
             Check out the upcoming events for Concordia Classical Academy. Click
             below to view our full calendar of events.
           </p>
           <Button
-            label='Events Calendar'
-            size='small'
-            theme='primary'
+            label="Events Calendar"
+            size="small"
+            theme="primary"
             startIcon={faCalendarDays}
-            link='/events'
+            link="/events/calendar"
           />
         </div>
-        <div className='w-full grid grid-cols-1 lg:grid-cols-3 gap-12 mt-8'>
+        <div className="w-full grid grid-cols-1 lg:grid-cols-3 gap-12 mt-8">
           {data.events.nodes
             .filter((event: any) => event.startDate > new Date().toISOString())
             .slice(0, 3)

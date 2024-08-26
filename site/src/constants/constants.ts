060ff388670506b27c16bdc11e06176d9528476d
Dan Choiniere <dchoinie@gmail.com>
2024-08-26 14:32:51 -0500

    Update portal url

--- site/src/constants/constants.ts
+++ site/src/constants/constants.ts
@@ -1,63 +1,78 @@
-const LOGIN = 'https://cca-mn.client.renweb.com/oa/login.cfm?memberid=16976'
-const REQUEST_INFO = 'https://cca-mn.client.renweb.com/oa/inquiry.cfm?memberid=16976'
-const ADMISSIONS = 'https://cca-mn.client.renweb.com/oa/index.cfm?memberid=16976'
-const ADOPT1 = 'https://donate.stripe.com/28o01n9ML4Z6dhK000'
-const ADOPT2 = 'https://donate.stripe.com/7sIcO99MLajqelO289'
-const ADOPT3 = 'https://donate.stripe.com/aEUcO9f758bi91u9AC'
-const ADOPT4 = 'https://donate.stripe.com/5kA5lH6Az0IQgtWbIL'
-const ADOPT5 = 'https://donate.stripe.com/7sI3dz8IH4Z6elO9AE'
-const ADOPT6 = 'https://donate.stripe.com/14k3dzf75bnufpSfZ3'
-const DONATE_GENERAL = 'https://donate.stripe.com/eVa01n4sr4Z62D68wC'
-const DONATE10 = 'https://donate.stripe.com/9AQ8xT6Azajq3Ha3cj'
-const DONATE20 = 'https://donate.stripe.com/6oE9BXaQP1MU0uY14c'
-const DONATE50 = 'https://donate.stripe.com/8wM6pLbUTdvC3HafZ7'
-const DONATE100 = 'https://donate.stripe.com/3cscO9f753V21z200a'
-const DONATE250 = 'https://donate.stripe.com/7sIaG1cYX0IQb9C28j'
-const DONATE500 = 'https://donate.stripe.com/28oaG1bUTezG5Pi4gs'
-const BUILDING_FOR_THE_FUTURE_CONTRIBUTION_ONE_TIME = 'https://donate.stripe.com/3csdSd0cb4Z6dhK8wJ'
-const BUILDING_FOR_THE_FUTURE_LEVEL_1 = "https://buy.stripe.com/3cs4hDf75fDKelO5ky"
-const BUILDING_FOR_THE_FUTURE_LEVEL_2 = "https://buy.stripe.com/bIYg0lf75bnu2D6fZd"
-const BUILDING_FOR_THE_FUTURE_LEVEL_3 = "https://buy.stripe.com/3csdSdgb9ezGa5y14k"
-const BUILDING_FOR_THE_FUTURE_LEVEL_4 = "https://buy.stripe.com/4gw7tP6AzdvC5Pi9AR"
-const BUILDING_FOR_THE_FUTURE_LEVEL_5 = "https://buy.stripe.com/aEUcO95wvgHOa5y8wO"
-const BUILDING_FOR_THE_FUTURE_LEVEL_6 = "https://buy.stripe.com/9AQ4hDbUT2QY5PibJ1"
-const CUSTOMER_PORTAL = "https://billing.stripe.com/p/login/dR62aQ6vU76UfMk8ww"
-const BUILDING_FOR_THE_FUTURE_LEVEL_1_TEST = "https://buy.stripe.com/test_8wM3ee3Q62na0Zq8wJ"
-const BUILDING_FOR_THE_FUTURE_LEVEL_2_TEST = "https://buy.stripe.com/test_bIYaGGdqGd1O7nO14i"
-const BUILDING_FOR_THE_FUTURE_LEVEL_3_TEST = "https://buy.stripe.com/test_dR66qqfyO9PCdMc7sH"
-const BUILDING_FOR_THE_FUTURE_LEVEL_4_TEST = "https://buy.stripe.com/test_fZe4iifyOge08rS5kA"
-const BUILDING_FOR_THE_FUTURE_LEVEL_5_TEST = "https://buy.stripe.com/test_aEU022aeu5zm0ZqdR7"
-const BUILDING_FOR_THE_FUTURE_LEVEL_6_TEST = "https://buy.stripe.com/test_cN2022biy0f29vW00i"
+const LOGIN = "https://factsmgt.com/login/";
+const REQUEST_INFO =
+  "https://cca-mn.client.renweb.com/oa/inquiry.cfm?memberid=16976";
+const ADMISSIONS =
+  "https://cca-mn.client.renweb.com/oa/index.cfm?memberid=16976";
+const ADOPT1 = "https://donate.stripe.com/28o01n9ML4Z6dhK000";
+const ADOPT2 = "https://donate.stripe.com/7sIcO99MLajqelO289";
+const ADOPT3 = "https://donate.stripe.com/aEUcO9f758bi91u9AC";
+const ADOPT4 = "https://donate.stripe.com/5kA5lH6Az0IQgtWbIL";
+const ADOPT5 = "https://donate.stripe.com/7sI3dz8IH4Z6elO9AE";
+const ADOPT6 = "https://donate.stripe.com/14k3dzf75bnufpSfZ3";
+const DONATE_GENERAL = "https://donate.stripe.com/eVa01n4sr4Z62D68wC";
+const DONATE10 = "https://donate.stripe.com/9AQ8xT6Azajq3Ha3cj";
+const DONATE20 = "https://donate.stripe.com/6oE9BXaQP1MU0uY14c";
+const DONATE50 = "https://donate.stripe.com/8wM6pLbUTdvC3HafZ7";
+const DONATE100 = "https://donate.stripe.com/3cscO9f753V21z200a";
+const DONATE250 = "https://donate.stripe.com/7sIaG1cYX0IQb9C28j";
+const DONATE500 = "https://donate.stripe.com/28oaG1bUTezG5Pi4gs";
+const BUILDING_FOR_THE_FUTURE_CONTRIBUTION_ONE_TIME =
+  "https://donate.stripe.com/3csdSd0cb4Z6dhK8wJ";
+const BUILDING_FOR_THE_FUTURE_LEVEL_1 =
+  "https://buy.stripe.com/3cs4hDf75fDKelO5ky";
+const BUILDING_FOR_THE_FUTURE_LEVEL_2 =
+  "https://buy.stripe.com/bIYg0lf75bnu2D6fZd";
+const BUILDING_FOR_THE_FUTURE_LEVEL_3 =
+  "https://buy.stripe.com/3csdSdgb9ezGa5y14k";
+const BUILDING_FOR_THE_FUTURE_LEVEL_4 =
+  "https://buy.stripe.com/4gw7tP6AzdvC5Pi9AR";
+const BUILDING_FOR_THE_FUTURE_LEVEL_5 =
+  "https://buy.stripe.com/aEUcO95wvgHOa5y8wO";
+const BUILDING_FOR_THE_FUTURE_LEVEL_6 =
+  "https://buy.stripe.com/9AQ4hDbUT2QY5PibJ1";
+const CUSTOMER_PORTAL = "https://billing.stripe.com/p/login/dR62aQ6vU76UfMk8ww";
+const BUILDING_FOR_THE_FUTURE_LEVEL_1_TEST =
+  "https://buy.stripe.com/test_8wM3ee3Q62na0Zq8wJ";
+const BUILDING_FOR_THE_FUTURE_LEVEL_2_TEST =
+  "https://buy.stripe.com/test_bIYaGGdqGd1O7nO14i";
+const BUILDING_FOR_THE_FUTURE_LEVEL_3_TEST =
+  "https://buy.stripe.com/test_dR66qqfyO9PCdMc7sH";
+const BUILDING_FOR_THE_FUTURE_LEVEL_4_TEST =
+  "https://buy.stripe.com/test_fZe4iifyOge08rS5kA";
+const BUILDING_FOR_THE_FUTURE_LEVEL_5_TEST =
+  "https://buy.stripe.com/test_aEU022aeu5zm0ZqdR7";
+const BUILDING_FOR_THE_FUTURE_LEVEL_6_TEST =
+  "https://buy.stripe.com/test_cN2022biy0f29vW00i";
 
 export default {
-    LOGIN,
-    REQUEST_INFO,
-    ADMISSIONS,
-    ADOPT1,
-    ADOPT2,
-    ADOPT3,
-    ADOPT4,
-    ADOPT5,
-    ADOPT6,
-    DONATE_GENERAL,
-    DONATE10,
-    DONATE20,
-    DONATE50,
-    DONATE100,
-    DONATE250,
-    DONATE500,
-    BUILDING_FOR_THE_FUTURE_CONTRIBUTION_ONE_TIME,
-    BUILDING_FOR_THE_FUTURE_LEVEL_1,
-    BUILDING_FOR_THE_FUTURE_LEVEL_2,
-    BUILDING_FOR_THE_FUTURE_LEVEL_3,
-    BUILDING_FOR_THE_FUTURE_LEVEL_4,
-    BUILDING_FOR_THE_FUTURE_LEVEL_5,
-    BUILDING_FOR_THE_FUTURE_LEVEL_6,
-    CUSTOMER_PORTAL,
-    BUILDING_FOR_THE_FUTURE_LEVEL_1_TEST,
-    BUILDING_FOR_THE_FUTURE_LEVEL_2_TEST,
-    BUILDING_FOR_THE_FUTURE_LEVEL_3_TEST,
-    BUILDING_FOR_THE_FUTURE_LEVEL_4_TEST,
-    BUILDING_FOR_THE_FUTURE_LEVEL_5_TEST,
-    BUILDING_FOR_THE_FUTURE_LEVEL_6_TEST,
-}
+  LOGIN,
+  REQUEST_INFO,
+  ADMISSIONS,
+  ADOPT1,
+  ADOPT2,
+  ADOPT3,
+  ADOPT4,
+  ADOPT5,
+  ADOPT6,
+  DONATE_GENERAL,
+  DONATE10,
+  DONATE20,
+  DONATE50,
+  DONATE100,
+  DONATE250,
+  DONATE500,
+  BUILDING_FOR_THE_FUTURE_CONTRIBUTION_ONE_TIME,
+  BUILDING_FOR_THE_FUTURE_LEVEL_1,
+  BUILDING_FOR_THE_FUTURE_LEVEL_2,
+  BUILDING_FOR_THE_FUTURE_LEVEL_3,
+  BUILDING_FOR_THE_FUTURE_LEVEL_4,
+  BUILDING_FOR_THE_FUTURE_LEVEL_5,
+  BUILDING_FOR_THE_FUTURE_LEVEL_6,
+  CUSTOMER_PORTAL,
+  BUILDING_FOR_THE_FUTURE_LEVEL_1_TEST,
+  BUILDING_FOR_THE_FUTURE_LEVEL_2_TEST,
+  BUILDING_FOR_THE_FUTURE_LEVEL_3_TEST,
+  BUILDING_FOR_THE_FUTURE_LEVEL_4_TEST,
+  BUILDING_FOR_THE_FUTURE_LEVEL_5_TEST,
+  BUILDING_FOR_THE_FUTURE_LEVEL_6_TEST,
+};
